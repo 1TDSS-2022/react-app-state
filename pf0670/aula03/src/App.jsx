ea3ef5a348b9cecc92b9ff29bf9996917b6db84f
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-08-12 20:14:32 -0300

    Exemplos CSS Objetos

---
+++ pf0670/aula03/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import './App.css'
+
+export default function App() {
+
+    const paragEstilo = {
+        color: 'red',
+        fontSize: '1.8em',
+        fontFamily: 'Courrier New',
+        textIndent: '50px',
+        fontStyle: 'italic'
+    }
+
+    return (
+        <div>
+            <h1 style={{ backgroundColor: '#fff234', color: '#0000ff', textAlign: 'center' }}>ESTUDOS SOBRE STATE</h1>
+            <p style={paragEstilo}>Rosas são vermelhas, violetas são azuis!</p>
+            <p className="classe1">Meu conteúdo de classe</p>
+
+        </div>
+    )
+}
