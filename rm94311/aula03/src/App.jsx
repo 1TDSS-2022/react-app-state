a4b26b62b13ac27033de0709fd119a5b35c67eb6
M4rcosHBS <marcosbilobram@hotmail.com>
2022-08-12 21:00:33 -0300

    UseStateFandS

--- rm94311/aula03/src/App.jsx
+++ rm94311/aula03/src/App.jsx
@@ -1,21 +1,15 @@
 import React from 'react'
 import './App.css'
+import TesteState from './components/TesteState'
 
 export default function App(){
-
-    const paragEstilo = {
-        color:'red',
-        fontSize:'1.8em',
-        fontFamily: 'Courrier New',
-        textIndent:'50px',
-        fontStyle: 'italic'
-
-    }
     return(
         <div>
             <h1 style={{backgroundColor:'#fff234', color:'#0000ff', textAlign: 'center'}}>ESTUDOS SOBRE STATE</h1>
-            <p style={{paragEstilo}}>Rosas são Rosas</p>
-            <p className='classe1'>Meu conteúdo de classe</p>
+            <div>
+                <h2>COMPONETE USANDO STATE</h2>
+                <TesteState/>
+            </div>
         </div>
     )
 }
---
+++ rm94311/aula03/src/components/TesteState.jsx
@@ -0,0 +1,29 @@
+import React, {useState} from "react";
+import TesteStateFilho from "./testeStateFilho/TesteStateFilho";
+
+export default function TesteState(){
+
+    const [valorState, setvalorState] = useState(5)
+    let valorVariavel = 5
+
+    const aumentar = ()=>{
+        //Utilizando o useState
+        setvalorState(valorState + 5)
+        valorVariavel+= 5
+        console.log(valorVariavel)
+    }
+
+    return (
+        <>
+            <h2>EXEMPLO STATE</h2>
+            <div>
+                <p>Valor do state = {valorState}</p>
+                <p>Valor da variável = {valorVariavel}</p>
+                <button onClick={() => aumentar()}>aumentar</button>
+            </div>
+            <TesteStateFilho 
+            valorProps={valorState} 
+            aumentarProps={aumentar} />
+      </>
+    )
+}
