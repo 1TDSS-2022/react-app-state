1c4303d7219734797fa0485b5a79dc277454c8ca
krinaf <rm93024@fiap.com.br>
2022-08-12 21:16:51 -0300

    Correção App.jsx

--- rm93024/aula03/src/App.jsx
+++ rm93024/aula03/src/App.jsx
@@ -1,21 +1,25 @@
 import React from "react"
 import './App.css'
+import TesteState from "./componentes/TesteState"
 
 export default function App() {
 
-    const paragEstilo = {
+    /*const paragEstilo = {
         color:'rgb(197, 180, 206)',
         fontSize:'1.8em',
         fontFamily: 'Courrier New',
         textIndent:'50px',
         fontStyle:'italic'
-    }
+    }*/
 
     return(
         <div>
-            <h1 style={{backgroundColor:'rgb(197, 180, 206)', color:'#160033', textAlign:'center'}}>ESTUDOS SOBRE STATE</h1>
-            <p style={paragEstilo}>Rosas são vermelhas, violetas são azuis!</p>        
-            <p className="classe1">Meu conteúdo de classe</p>        
+            <h1 style={{backgroundColor:'rgb(197, 180, 206)', color:'#160033',
+            textAlign:'center'}}>ESTUDOS SOBRE STATE</h1>
+            <div>
+                <h2>COMPONEN USANDO STATE</h2>
+                <TesteState />
+            </div>     
         </div>
     )
 }
