2ae25354de7e034ff5548cdda24240d7bc98f35a
WesleyKleim <wesleykleim@gmail.com>
2022-08-29 15:31:10 -0300

    Exemplo Css Objetos

--- rm94412/aula03/src/App.jsx
+++ rm94412/aula03/src/App.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import './App.css'
+const paragrafoEstilo={
+    color: 'blue',
+    fontSize: '1.8em',
+    fontFamily: ' Courrier New',
+    textIndent: '50px',
+    fontStyle: 'italic'
+}
 
 export default function App(){
     return(
         <div>
-            <h1>Estudos Sobre STATE</h1>
+            <h1 style={{backgroundColor:'#fff234', color:'	#4B0082'}}>Estudos Sobre STATE</h1>
+            <p style={paragrafoEstilo}>Estou aprendendo a progamar em React com CSS inline</p>
+            <p className="classe1">Meu conteúdo de classe</p>
         </div>
     )
 }
